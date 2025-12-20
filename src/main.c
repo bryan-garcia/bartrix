04a456bcc1b8c9dc0146a5b9c4bc924cd41353e5
bryan-garcia <bgarci26@ucsc.edu>
2025-12-20 14:35:41 -0800

    update platformio.ini. messing with malloc

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "driver/gpio.h"
+#include "stdio.h"
 
 #define BLINK_GPIO 2
 
@@ -9,11 +10,24 @@ void app_main(void)
     esp_rom_gpio_pad_select_gpio(BLINK_GPIO);
     gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
 
+    int allocated = 0;
+    int AMOUNT = 1000;
+    printf("Hello world\n");
+
     while (1)
     {
-        gpio_set_level(BLINK_GPIO, 1);
-        vTaskDelay(500 / portTICK_PERIOD_MS);
-        gpio_set_level(BLINK_GPIO, 0);
-        vTaskDelay(500 / portTICK_PERIOD_MS);
+        // gpio_set_level(BLINK_GPIO, 1);
+        // vTaskDelay(100 / portTICK_PERIOD_MS);
+        // gpio_set_level(BLINK_GPIO, 0);
+        // vTaskDelay(100 / portTICK_PERIOD_MS);
+
+        int addr = (int) malloc(AMOUNT);
+
+        allocated += AMOUNT;
+        printf("allocated %i\n", allocated);
+
+        if (addr == 0) {
+            return;
+        }
     }
 }
